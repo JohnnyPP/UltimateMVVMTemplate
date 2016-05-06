7c4eed5e85c2ef6792235ec15c4556d8430c95e7
Johnny P <johnnnyppp@gmail.com>
2016-05-06 15:02:15 +0200

    Initial release

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class ViewModel
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+
+        public ViewModel()
+        {
+            Model = new Model.Model();
+            ButtonClick = new Helper.ActionCommand(ButtonClickCommand);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Model.Model Model { get; set; }
+        public Helper.ActionCommand ButtonClick { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        private void ButtonClickCommand()
+        {
+            Model.IncrementTextBox();
+        }
+
+        #endregion
+    }
+}
